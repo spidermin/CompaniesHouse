8ab00205201efc8fecb9999f84c3db85e88af3aa
spidermin <54328916+spidermin@users.noreply.github.com>
2019-08-20 19:59:53 +0100

    Search form added
    Style sheet link may need to be changed.

--- index.php
+++ index.php
@@ -9,3 +9,17 @@
 /// Included files... 
 	include '/functions.php';
 ?>
+
+<!-- Search form --> 
+<body>
+	<div id="main-container">
+<h1>Companies House credit approval system</h1>
+<hr>
+<h2>Search Companies House records</h2>
+<form id="search" method="post" action=""> 
+	<input type="number" name="search_term" id="search_term" placeholder="Search company number"/>
+	<input type="submit" name="submit" id="submit" value="submit"   />
+</form>
+<hr> 
+	
+	
