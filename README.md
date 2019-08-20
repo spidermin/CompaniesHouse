7b56bc29d2b6c94580753d81519cdd1d9591bf7d
spidermin <54328916+spidermin@users.noreply.github.com>
2019-08-20 20:37:07 +0100

    curl method used to call api
    printed json to see all results

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Brief:
 Using the Companies House public API (https://developer.companieshouse.gov.uk/api/docs/index.html), create a small app using your choice of PHP/Javascript that takes a user input for a company number, validates this input, then uses the API to verify whether the company is currently active, and whether it has any history of insolvency.
 
 If the company is not active, return a message to the user refusing a credit account application, if the company is active but has a history of insolvency return a risk score (between 1-100) establishing how high the risk of issuing a credit account might be.
+
+Build: 
+
+In order to get the required data two API calls are needed. One being company search and the other being insolvency information. 
+
+
+
+
+Run: 
+
+
+Assumptions: 
--- functions.php
+++ functions.php
@@ -13,3 +13,29 @@ $number = $_POST['search_term'];
 $form_result = $_POST['submit'];
 
 //echo $trimmed_search;
+
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, 'https://api.companieshouse.gov.uk/search/companies?q='.$trimmed_search);
+//curl_setopt($curl, CURLOPT_URL, 'https://api.companieshouse.gov.uk/company/'.$trimmed_search.'/insolvency');
+curl_setopt($curl, CURLOPT_USERPWD,"m_zHeLJNvJhlw9aRn0UL5aoGSylrOMzJ0-anvuJd");
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET'); 
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt ($curl, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($curl);
+if($errno = curl_errno($curl)) {
+$error_message = curl_strerror($errno);
+echo "cURL error ({$errno}):\n {$error_message}";
+}
+curl_close($curl);
+
+$json = json_decode($response, true); // decode the JSON into an associative array
+
+
+// Handle the API response below here... 
+echo '<p><pre>' . print_r($json, true) . '</pre></p>';
+
+}
