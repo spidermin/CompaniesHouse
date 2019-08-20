1d63225342b83491b920a5a5f0cce7933013c311
spidermin <54328916+spidermin@users.noreply.github.com>
2019-08-20 20:16:11 +0100

    Start of function build
    search term vars added and stripped to remove spaces as API does not recognise them. POST vars also added

--- functions.php
+++ functions.php
@@ -1,1 +1,15 @@
+<?php
 
+function Callapi() {
+
+
+$search_term =  $_REQUEST['search_term']; //Get search term...
+
+$trimmed_search = str_replace(' ', '', $search_term); //Remove Spaces...
+
+///Declare vars...
+
+$number = $_POST['search_term'];
+$form_result = $_POST['submit'];
+
+//echo $trimmed_search;
