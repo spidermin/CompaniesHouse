cf261650f09477d69c79558f950ebd584ecb9554
spidermin <54328916+spidermin@users.noreply.github.com>
2019-08-20 21:26:18 +0100

    test print all data

--- functions.php
+++ functions.php
@@ -95,6 +95,6 @@ curl_close($curl);
 $json = json_decode($response, true); // decode the JSON into an associative array
 
 // Handle the API response below here... 
-//echo '<p><pre>' . print_r($json, true) . '</pre></p>';
+echo '<p><pre>' . print_r($json, true) . '</pre></p>';
 
 };
